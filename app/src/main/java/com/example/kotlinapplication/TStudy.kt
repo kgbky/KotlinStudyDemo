e8b082ee8c70a91ec74fdaa53275c170ff9c0fe5
andy.zheng <andy.zheng@uptech.ai>
2024-01-25 14:53:10 +0800

    复习 Kotlin 泛型

--- app/src/main/java/com/example/kotlinapplication/TStudy.kt
+++ app/src/main/java/com/example/kotlinapplication/TStudy.kt
@@ -1,10 +1,18 @@
 package com.example.kotlinapplication
 
+import java.util.Date
+
 /**
  * Created by lenovo on 2023/5/29    17:47
  *
- * 创造泛型的初衷是为了保证类型安全，所以泛型是不变的
+ * 创造泛型的初衷是为了保证类型安全，所以泛型是不型变的
  * List<String> 和 List<Object> 并没有子父类的关系
+ *
+ * 需要型变的时候可以使用 out 和 in 关键字:
+ *
+ * in 对应 java 中  ? super String，表示 Sting 及其父类。能写不能读(数据)
+ *
+ * out 对应 java 中  ? extends String，表示 String 及其之类。能读不能写(数据)
  */
 class TStudy {
 
@@ -22,13 +30,30 @@ class TStudy {
 
     }
 
+    //指定泛型的范围 Number及其子类。NumberList 可以存Int 或 double
+    //但是不会既存Int、又存double
+    class NumberList<T : Number> : ArrayList<T>() {
+
+        //输出方法
+        fun find(t: T): Int {
+            return indexOf(t)
+        }
+
+        //输入方法
+        fun changeZero(value: T) {
+            set(0, value)
+        }
+
+    }
+
     //指定泛型的范围 Number及其子类
     fun <T : Number> sum2(a: T, b: T): T {
         return a
     }
 
-    //指定泛型的范围 Number及其子类
-    fun <T> sum3(a: T): T where T : Number {
+    //指定泛型的范围，Where子句可以指定多个上界
+    //Number及其子类，CharSequence及其子类
+    fun <T> sum3(a: T): T where T : Number, T : CharSequence {
         return a
     }
 
@@ -52,7 +77,7 @@ class TStudy {
         val strList: List<String> = ArrayList<String>()
         val strList2: MutableList<String> = ArrayList<String>()
         //类型通配符：Kotlin(*)   java(?)
-        val list:MutableList<*> = mutableListOf(1,"kotlin")
+        val list: MutableList<*> = mutableListOf(1, "kotlin", Date())
     }
 
     //扩展函数
@@ -60,7 +85,46 @@ class TStudy {
         return indexOf(t)
     }
 
-    //in 对应 java 中  ? super String，表示 Sting 及其父类。能存不能取
-    //out 对应 java 中  ? extends String，表示 String 及其之类。能取不能存
+    //in 对应 java 中  ? super String，表示 Sting 及其父类。消费者
+    //out 对应 java 中  ? extends String，表示 String 及其子类。生产者
+
+    //声明处型变
+    interface Source<out T> {
+        fun nextT(): T
+    }
+
+    fun demo(strs: Source<String>) {
+        // 这个没问题，因为 T 是一个 out-参数
+        val objects: Source<CharSequence> = strs
+    }
+
+    //声明处型变
+    interface Comparable<in T> {
+        operator fun compareTo(other: T): Int
+    }
+
+    fun demo(x: Comparable<Number>) {
+        x.compareTo(1.0) // 1.0 拥有类型 Double，它是 Number 的子类型
+        val y: Comparable<Double> = x // 可以将 x 赋给类型为 Comparable <Double> 的变量
+    }
+
+    //使用处型变 对应于 Java 的 Array<? extends Object>
+    fun copy(from: Array<out Any>, to: Array<Any>) {
+        assert(from.size == to.size)
+        for (i in from.indices)
+            to[i] = from[i]
+    }
+
+    //使用处型变  对应于 Java 的 Array<? super String>
+    fun fill(dest: Array<in String>, value: String) {
+        //可以传递一个 CharSequence 数组或一个 Object 数组给 fill() 函数
+    }
+
+    fun demo() {
+        val ints: Array<Int> = arrayOf(1, 2, 3)
+        val any = Array<Any>(3) { "" }
+        copy(ints, any)
+        //   ^ 其类型为 Array<Int> 但此处期望 Array<Any>
+    }
 
 }
